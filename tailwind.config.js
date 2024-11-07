0418dc028b2b527fba5621f996d9e959a4339467
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2024-11-08 03:00:13 +0330

    mobile appbar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["vazir"],
+        sans: ["IRANSansX"],
       },
     },
   },
